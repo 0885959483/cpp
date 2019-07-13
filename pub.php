97c901aeae62cc93942d55c9a8ac773b80b908c0
0885959483 <35373284+0885959483@users.noreply.github.com>
2019-07-13 22:38:37 +0700

    Update pub.php

--- pub.php
+++ pub.php
@@ -37,7 +37,7 @@
      
     curl_setopt($ch, CURLOPT_POSTFIELDS, $tmsg);
  
-    //curl_setopt($ch, CURLOPT_USERPWD, "mJ7K4MfteC7p0dW:pp4gzMhCvJIqlxc66hKEvk46m");
+    curl_setopt($ch, CURLOPT_USERPWD, "BjwsEhVUWt50Sxy:TlUva5etzHhiOCPUSAA6w794a");
      
     $response = curl_exec($ch);
     
